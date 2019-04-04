16318547ebf40d9917313b1b0a6c4ad9ed1f73e1
Michael Peters <michaelpeterswa@icloud.com>
2019-04-04 12:46:50 -0700

    first commit

--- src/main/java/io/github/michaelpeterswa/testplugin/testPlugin.java
+++ src/main/java/io/github/michaelpeterswa/testplugin/testPlugin.java
@@ -13,8 +13,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class testPlugin extends JavaPlugin {
 @Override
 public void onEnable() {
-        // TODO Insert logic to be performed when the plugin is enabled
-
         getLogger().info("peenPlugin has been enabled");
     }
 
