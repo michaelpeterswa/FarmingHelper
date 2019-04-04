13f11532b356433fde58e3b01342fbea4d09ae23
Michael Peters <michaelpeterswa@icloud.com>
2019-04-04 12:40:00 -0700

    first commit

--- src/main/java/io/github/michaelpeterswa/testplugin/testPlugin.java
+++ src/main/java/io/github/michaelpeterswa/testplugin/testPlugin.java
@@ -27,19 +27,20 @@ public void onDisable() {
 public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     if (cmd.getName().equalsIgnoreCase("peen")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
         if(!(sender instanceof Player)) {
-            Player player = this.getServer().getPlayer(args[0]);
-            ItemStack item1 = new ItemStack(Material.STICK,1);
-            ItemMeta meta = item1.getItemMeta();
-            meta.setDisplayName("thicc peen");
-            item1.addEnchantment(Enchantment.DURABILITY, 2);
-            item1.setItemMeta(meta);
-            player.getInventory().addItem(item1);
+            sender.sendMessage("You must be a player to execute that command!");
+            return true;
         }
     }
     //If this has happened the function will return true.
     // If this hasn't happened the value of false will be returned.
+    Player player = (Player) sender;
+    ItemStack item1 = new ItemStack(Material.WOODEN_HOE,1);
+    ItemMeta meta = item1.getItemMeta();
+    meta.setDisplayName("§c§lr§6§la§e§li§a§ln§b§lb§d§lo§c§lw peen");
+    meta.addEnchant(Enchantment.DURABILITY, 1, true);
+    item1.setItemMeta(meta);
+    player.getInventory().addItem(item1);
 
-
-    return false;
+    return true;
     }
 }
